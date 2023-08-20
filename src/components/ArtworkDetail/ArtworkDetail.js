f383a0072cad02c418c62b017ec095cbc9a7ad70
kubraguler <oktnkbr@gmail.com>
2023-08-20 12:02:01 +0100

    Edit detail page styles

--- src/components/ArtworkDetail/ArtworkDetail.js
+++ src/components/ArtworkDetail/ArtworkDetail.js
@@ -6,23 +6,57 @@ const ArtworkDetail = () => {
 	const { selectedArtwork: artwork } = useSelector((state) => state.artworks);
 
 	return (
-		<div>
+		<div className="artwork--detail">
 			{artwork && (
 				<>
 					{artwork && (
 						<>
-							<p>Title: {artwork.title}</p>
-							<p>Place of Origin: {artwork.place_of_origin}</p>
-							<p>Inscriptions : {artwork.inscriptions}</p>
-							<p>Artist Display : {artwork.artist_display}</p>
-							<p>Theme Titles : {artwork.theme_titles}</p>
 							<img
 								className="artwork--list-item_img"
-								src={artwork?.thumbnail.lqip}
-								alt={artwork?.thumbnail.alt_text}
+								src={artwork.thumbnail.lqip}
+								alt={artwork.thumbnail.alt_text}
 							/>
-							<p>Provenance Text: {artwork.provenance_text}</p>
-							<p>Publication History : {artwork.publication_history}</p>
+							{artwork.title && (
+								<p className="artwork--detail-title">
+									<b>Title:</b> {artwork.title}
+								</p>
+							)}
+
+							{artwork.place_of_origin && (
+								<p>
+									<b>Place of Origin:</b> {artwork.place_of_origin}
+								</p>
+							)}
+
+							{artwork.inscriptions && (
+								<p>
+									<b>Inscriptions :</b> {artwork.inscriptions}
+								</p>
+							)}
+
+							{artwork.artist_display && (
+								<p>
+									<b>Artist Display :</b> {artwork.artist_display}
+								</p>
+							)}
+
+							{artwork.theme_titles && (
+								<p>
+									<b>Theme Titles :</b> {artwork.theme_titles}
+								</p>
+							)}
+
+							{artwork.provenance_text && (
+								<p>
+									<b>Provenance Text:</b> {artwork.provenance_text}
+								</p>
+							)}
+
+							{artwork.publication_history && (
+								<p className="artwork--detail-publication">
+									<b>Publication History :</b> {artwork.publication_history}
+								</p>
+							)}
 						</>
 					)}
 				</>
